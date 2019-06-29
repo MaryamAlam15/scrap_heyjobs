7a5f9a23b4d0b5ff02fc0a64dc8fd1aabe881d9c
Maryam Alam <maryamalam15@gmail.com>
2019-06-29 23:33:04 +0500

    - fixing regex.

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # PARSER
 HEYJOBS_JOB_URL = 'https://www.heyjobs.co/en-de/jobs'
 AD_ANCHOR = 'a[href^="/en-de/jobs/"]'
-UUID_REGEX = '/en-de/jobs/(.+?)?'
+UUID_REGEX = r'/en-de/jobs/(.+?)\?'
 TITLE = 'h2'
